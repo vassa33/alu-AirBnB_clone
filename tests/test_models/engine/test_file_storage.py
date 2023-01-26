83a89537d3b62351fe6ca975dc587f8d98c0d788
Abissa <a.sani@alustudent.com>
2023-01-26 13:06:07 +0200

    fix test_city and add file storage

--- tests/test_models/engine/test_file_storage.py
+++ tests/test_models/engine/test_file_storage.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 
+"""Unittest for FileStorage Class."""
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test cases for FileStorage class."""
+
+    def test_instance(self):
+        """test instance."""
+        storage = FileStorage()
+        self.assertIsInstance(storage, FileStorage)
+
+    def test_is_class(self):
+        """test instance."""
+        storage = FileStorage()
+        self.assertEqual(str(type(storage)),
+                         "<class 'models.engine.file_storage.FileStorage'>")
+
+
+if __name__ == "__main__":
+    unittest.main()
