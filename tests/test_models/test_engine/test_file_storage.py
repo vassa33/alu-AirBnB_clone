28348fbb21a12c22a813e07223d2676526e84f7d
Abissa <a.sani@alustudent.com>
2023-01-27 07:19:05 +0200

    fix: test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,47 +2,60 @@
 
 """Unittest for FileStorage Class."""
 
+import os
+import json
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
     """Test cases for FileStorage class."""
 
+    """setUp Method called to prepare the test fixture. 
+    This is called immediately before calling the test method """
+    file_path = 'file.json'
+
+    def setUp(self):
+        self.storage = FileStorage()
+
+    """Method to clean up resources that were created during the setup"""
+
+    def tearDown(self):
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+        del self.storage
+
     def test_instance(self):
         """test instance."""
-        storage = FileStorage()
-        self.assertIsInstance(storage, FileStorage)
+        self.assertIsInstance(self.storage, FileStorage)
 
     def test_is_class(self):
         """test instance."""
-        storage = FileStorage()
-        self.assertEqual(str(type(storage)),
+        self.assertEqual(str(type(self.storage)),
                          "<class 'models.engine.file_storage.FileStorage'>")
 
     def test_file_path(self):
-        pass
+        """test file.json exits"""
+        self.assertFalse(os.path.exists(self.file_path))
+        self.storage.save()
+        self.assertTrue(os.path.exists(self.file_path))
 
     def test_objects(self):
-        pass
+        self.assertEqual(self.storage.all(), {})
+        base = BaseModel()
+        self.storage.new(base)
+        self.assertIn(base, self.storage.all().values())
 
     def test_all(self):
-        pass
-
-    def test_new(self):
-        pass
-
-    def test_save(self):
-        pass
+        """ test all."""
+        self.assertEqual(type(self.storage.all()), dict)
+        self.assertEqual(self.storage.all(), {})
 
     def test_reload(self):
-        pass
-
-    def test_base_model_init_kwargs(self):
-        pass
-
-    def test_base_model_save(self):
-        pass
+        """test reload"""
+        self.storage.reload()
+        self.assertIsInstance(self.storage.all(), type({}))
 
 
 if __name__ == "__main__":
